e0846c0cd579fc08a69c644294d5024ae8c39104
Aragon Emanuel <aragon3m4@gmail.com>
2023-05-03 09:38:01 -0300

    Remove unused code

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -7,34 +7,3 @@ export function getOffset(URL) {
   }
   return offset;
 }
-
-export function getPokemonTypes(pokemon) {
-  let types = ''
-  try {
-    pokemon.forEach(pokemonTypes => {
-      types += `${pokemonTypes.type.name}` + ' '
-    })
-  } catch (e) {
-    types = '';
-  }
-  return types.toUpperCase();
-}
-
-export function capitalize(string) {
-  try {
-    string = string.charAt(0).toUpperCase() + string.slice(1);
-  } catch (e) {
-    //console.error(e)
-  }
-  return string;
-}
-
-export function uncapitalize(string) {
-  try {
-    string = string.charAt(0).toLowerCase() + string.slice(1);
-  } catch (e) {
-    //console.error(e)
-  }
-  return string;
-}
-
