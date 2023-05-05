ceb2b8a55ac794c480787ebef0bd14ee09fc7f4a
Aragon Emanuel <aragon3m4@gmail.com>
2023-05-05 06:53:11 -0300

    Update image files source location

--- src/ui/pokemonSection.js
+++ src/ui/pokemonSection.js
@@ -8,16 +8,19 @@ const $pokemonType = document.querySelector('#pokemon-type');
 const $pokemonHeight = document.querySelector('#pokemon-height');
 const $pokemonWeight = document.querySelector('#pokemon-weight');
 
-$pokemonImg.setAttribute('onerror', "javascript:this.src='src/ui/img/pokemonNotFound.png'")
+$pokemonImg.setAttribute('onerror', "javascript:this.src='src/assets/img/pokemonNotFound.png'")
 
 export function showPokemon(pokemon) {
+
     $pokemonName.textContent = (`${pokemon.name}`).toUpperCase();
     $pokemonID.textContent = (`ID: ${pokemon.id}`)
-    $pokemonImg.setAttribute(onerror, "this.src='./img/pokemonNotFound.png'")
+    $pokemonImg.setAttribute(onerror, "this.src='./assets/img/pokemonNotFound.png'")
     $pokemonImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
     $pokemonType.textContent = 'TYPE: ' + pokemon.types;
     $pokemonHeight.textContent = (`HEIGHT: ${pokemon.height / 10} M`).toUpperCase();
     $pokemonWeight.textContent = (`WEIGHT: ${pokemon.weight / 10} KG`).toUpperCase();
+    
+} 
 
 export async function setNewPokemon(pokemon) {
     showPokemon(await getPokemon(pokemon));
