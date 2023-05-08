39d8c29ddbd4c98472a1cc75c80a98e1a8328208
Aragon Emanuel <aragon3m4@gmail.com>
2023-05-08 08:35:53 -0300

    Use PokemonList object to change between pages

--- src/ui/pageSelector.js
+++ src/ui/pageSelector.js
@@ -1,5 +1,5 @@
 import { getPokemons } from '../storage/pokemon.js';
-import { setNewPokemonGrid } from './grid.js';
+import { setNewPokemonGrid as changePage } from './grid.js';
 import { getOffset } from '../utilities/utilities.js';
 
 const POKEMONS_LIMIT = 16
@@ -23,7 +23,18 @@ function handleFormEvent(event) {
   event.preventDefault();
 }
 
+async function handlePageChange(offset) {
 
+  const pageSelected = await getPokemons(offset)
+  const {
+    nextUrl: next,
+    previousUrl: previous,
+  } = pageSelected;
+
+  previousPage = previous;
+  nextPage = next;
+
+  changePage(pageSelected);
 }
 
 export function setUpPageSelector(pokemonList) {
@@ -36,6 +47,8 @@ export function setUpPageSelector(pokemonList) {
   previousPage = previous;
   nextPage = next;
 
+  const PAGES = Math.ceil(POKEMONS / POKEMONS_LIMIT) + 1; //
+  for (let i = 1; i < PAGES; i++) { // 
     $pageSelector.appendChild(createSelectorElement(i));
   }
 }
@@ -51,7 +64,7 @@ async function showPreviousPage() {
   if (previousPage) {
     $previousBtn.classList.remove('is-error');
     $nextBtn.classList.remove('is-error');
-    setNewPokemonGrid(await getPokemons(getOffset(previousPage)));
+    await handlePageChange(getOffset(previousPage));
   } else {
     $previousBtn.classList.add('is-error');
   }
@@ -60,7 +73,7 @@ async function showPreviousPage() {
 async function showNextPage() {
   if (nextPage) {
     $previousBtn.classList.remove('is-error');
-    setNewPokemonGrid(await getPokemons(getOffset(nextPage)));
+    await handlePageChange(getOffset(nextPage));
   } else {
     $nextBtn.classList.add('is-error');
   }
