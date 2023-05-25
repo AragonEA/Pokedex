4d93cdc07d0fb72d41a3a8a46bcb26ebbb349238
Aragon Emanuel <aragon3m4@gmail.com>
2023-05-25 08:09:03 -0300

    Remove duplicated code

--- src/js/ui/pokemonSection.js
+++ src/js/ui/pokemonSection.js
@@ -7,14 +7,12 @@ const $pokemonType = document.querySelector('#pokemon-type');
 const $pokemonHeight = document.querySelector('#pokemon-height');
 const $pokemonWeight = document.querySelector('#pokemon-weight');
 
-$pokemonImg.setAttribute('onerror', "javascript:this.src='src/assets/img/pokemonNotFound.png'")
-
 export function showPokemon(pokemon) {
 
     $pokemonName.textContent = (`${pokemon.name}`).toUpperCase();
     $pokemonID.textContent = (`ID: ${pokemon.id}`)
-    $pokemonImg.setAttribute(onerror, "this.src='./assets/img/pokemonNotFound.png'")
     $pokemonImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+    $pokemonImg.setAttribute(onerror, "this.src='./assets/img/pokemonNotFound.png'")
     $pokemonType.textContent = 'TYPE: ' + pokemon.types;
     $pokemonHeight.textContent = (`HEIGHT: ${pokemon.height / 10} M`).toUpperCase();
     $pokemonWeight.textContent = (`WEIGHT: ${pokemon.weight / 10} KG`).toUpperCase();
