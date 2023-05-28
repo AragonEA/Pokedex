e60187dbb418299780dc101cc6159bd29db43e4c
Aragon Emanuel <aragon3m4@gmail.com>
2023-05-28 08:49:31 -0300

    Format

--- src/js/ui/grid.js
+++ src/js/ui/grid.js
@@ -7,24 +7,24 @@ export function setNewPokemonGrid(pokemonList) {
 
 function createPokemonGrid(pokemonList) {
     const $pokemonGrid = document.querySelector('#pokemon-grid');
-    
+
     for (let i = 0; i < (pokemonList.ids).length; i++) {
         const $pokemonContainer = document.createElement('div');
         $pokemonContainer.dataset.pokemonName = pokemonList.names[i];
         $pokemonContainer.classList = 'grid-item nes-pointer';
-        $pokemonContainer.appendChild(createIconElement(pokemonList.ids[i],pokemonList.names[i]));
+        $pokemonContainer.appendChild(createIconElement(pokemonList.ids[i], pokemonList.names[i]));
         $pokemonContainer.appendChild(createNameElement(pokemonList.names[i]));
         $pokemonGrid.appendChild($pokemonContainer);
-    } 
+    }
 }
 
-function createNameElement(pokemonName){
+function createNameElement(pokemonName) {
     const $pokemonName = document.createElement('p');
     $pokemonName.textContent = (pokemonName);
     return $pokemonName;
 }
 
-function createIconElement(id, pokemonName){
+function createIconElement(id, pokemonName) {
     const $pokemonIcon = document.createElement('img');
     $pokemonIcon.dataset.pokemonName = pokemonName;
     $pokemonIcon.classList = 'pokemon-icon'
