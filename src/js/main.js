cfee8c50ea9c79b848702e0a7adcbd9387151fd9
Aragon Emanuel <aragon3m4@gmail.com>
2023-05-28 08:29:39 -0300

    Use setNewPokemon instead of showPokemon

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import { getPokemons, getPokemon } from './storage/pokemon.js';
-import { showPokemon } from './ui/pokemonSection.js';
+import { getPokemons } from './storage/pokemon.js';
+import { setNewPokemon } from './ui/pokemonSection.js';
 import { setNewPokemonGrid } from './ui/grid.js'
 import { setUpPageSelector } from './ui/pageSelector.js'
 import { setUpModal } from './ui/modal.js'
@@ -7,7 +7,7 @@ import { setUpModal } from './ui/modal.js'
 async function initialize() {
     const pokemonList = await getPokemons();
     setNewPokemonGrid(pokemonList)
-    showPokemon(await getPokemon(pokemonList.names[0])); //Show data of the first pokemon in the grid
+    setNewPokemon(pokemonList.names[0]); //Show data of the first pokemon in the grid
     setUpModal();
     setUpPageSelector(pokemonList);
 }
