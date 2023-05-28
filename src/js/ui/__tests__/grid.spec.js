b31e7abe1d5d4cbd52a6441e8b755992687197d2
Aragon Emanuel <aragon3m4@gmail.com>
2023-05-28 09:06:14 -0300

    Remove unnecessary code

--- src/js/ui/__tests__/grid.spec.js
+++ src/js/ui/__tests__/grid.spec.js
@@ -6,50 +6,8 @@ import { setNewPokemonGrid } from '../grid';
 
 document.body.innerHTML = `
 <body>
-<header>
-    <img id="logo" src="./src/assets/img/pokemonLogo.png" alt="pokedex logo">
-</header>
 <main>
     <div class="container">
-
-        <section class="left">
-            <div id="pokemon-data" class="nes-container is-centered background-white border-radius">
-                <picture>
-                    <img id="pokemon-img" class="img" alt="pokemon-image">
-                </picture>
-                <p id="pokemon-name" class="title">NAME</p>
-                <ul class="nes-list is-disc" id="pokemon-stats">
-                    <li id="pokemon-id">ID: </li>
-                    <li id="pokemon-type">TYPE: </li>
-                    <li id="pokemon-height">HEIGHT: </li>
-                    <li id="pokemon-weight">WEIGHT: </li>
-                </ul>
-                <div>
-                    <button id="modal-btn" class="nes-btn">Show Abilities & Movements</button>
-                    <div class="modal">
-                        <div class="modal-content">
-                            <div class="modal-title">
-                                <span id="close-btn" class="close">&times;</span>
-                                <h2>Abilities</h2>
-                            </div>
-                            <div id="abilities-section" class="modal-data">
-                                <ul id="abilities">
-                                    <li>Loading...</li>
-                                </ul>
-                            </div>
-                            <div class="modal-title">
-                                <h2>Movements</h2>
-                            </div>
-                            <div id="movements-section" class="modal-data">
-                                <ul id="movements">
-                                    <li>Loading...</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
 
         <section class="right">
             <div id="pokemon-grid" class="nes-container is-centered background-white border-radius">
@@ -66,13 +24,9 @@ document.body.innerHTML = `
                 <button id="next-btn" type="button" class="nes-btn">Next</button>
             </nav>
         </section>
-
+        
     </div>
 </main>
-<footer>
-    <a href="https://github.com/AragonEA" target="_blank">AragonEA<i class="nes-icon github is-small"></i></a>
-</footer>
-<script src="src/js/main.js" type="module"></script>
 </body>`;
 
 const pokemonList = {
